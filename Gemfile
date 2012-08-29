1493253a5920a62e6ac10dea38822359df4b85c0
Marc Schuba <marc.schuba@mobileeventguide.de>
2012-08-29 10:30:08 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'meg-jsontoolkit', git:'https://github.com/mobileeventguide/meg-jsontoolkit.git'
+gem 'nokogiri'
+gem 'pry'
---
+++ inline_images.rb
@@ -0,0 +1,12 @@
+require 'meg-jsontoolkit'
+require 'nokogiri'
+require 'pry'
+
+doc = Nokogiri::HTML IO.read "./test.html"
+doc.css('img').each do |image|
+  source_path = image['src'].gsub 'file://localhost', ''
+  image_data = Meg::Jsontoolkit.base64_encode_file source_path
+  image['src'] = image_data if image_data
+end
+
+IO.write 'test2.html', doc.to_s

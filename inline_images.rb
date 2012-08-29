25dc2016ed2425b17f0b62b0288eaca807109b43
Marc Schuba <marc.schuba@mobileeventguide.de>
2012-08-29 10:39:21 +0200

    remove tables
    they will break the word wrapping if too large

--- inline_images.rb
+++ inline_images.rb
@@ -3,10 +3,15 @@ require 'nokogiri'
 require 'pry'
 
 doc = Nokogiri::HTML IO.read "./test.html"
+
 doc.css('img').each do |image|
   source_path = image['src'].gsub 'file://localhost', ''
   image_data = Meg::Jsontoolkit.base64_encode_file source_path
   image['src'] = image_data if image_data
+end
+
+doc.css('table').each do |table|
+  table.remove
 end
 
 IO.write 'test2.html', doc.to_s

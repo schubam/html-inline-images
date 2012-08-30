0b7c46806f667ca03591d5a4203676bb719a34e6
Marc Schuba <marc.schuba@mobileeventguide.de>
2012-08-30 15:13:02 +0200

    image width constraints

--- inline_images.rb
+++ inline_images.rb
@@ -2,16 +2,19 @@ require 'meg-jsontoolkit'
 require 'nokogiri'
 require 'pry'
 
-doc = Nokogiri::HTML IO.read "./test.html"
+doc = Nokogiri::HTML IO.read "./test2.html"
 
 doc.css('img').each do |image|
   source_path = image['src'].gsub 'file://localhost', ''
   image_data = Meg::Jsontoolkit.base64_encode_file source_path
-  image['src'] = image_data if image_data
+  if image_data
+    image['src'] = image_data 
+    image['style'] = 'max-width:300px;'
+  end
 end
 
 doc.css('table').each do |table|
   table.remove
 end
 
-IO.write 'test2.html', doc.to_s
+IO.write 'test3.html', doc.to_s
